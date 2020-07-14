d8f3a3637d4796e2cba7e7cbe7c51ff9825a4fc1
gunh0 <devgun@kakao.com>
2020-07-14 13:54:42 +0900

    modify UI, fin. load ip QLineEdit text

--- src/main_window.py
+++ src/main_window.py
@@ -1,56 +1,31 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 
 # import .ui file
 # .ui file must be located in the same directory as the Python code file.
 form_class = uic.loadUiType("CC_Server_GUI.ui")[0]
 
-# GUI Class Define
-class WindowClass(QMainWindow, form_class):
+
+class WindowClass(QtWidgets.QMainWindow, form_class):   # GUI Class Define
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.btn_100.setCheckable(True)
-        self.btn_100.clicked.connect(self.btn_100_function)
+        self.pBtn100_collect.clicked.connect(self.pBtn100_collect_function)
 
-        self.btn_200.setCheckable(True)
-        self.btn_200.clicked.connect(self.btn_200_function)
+    ip_list = []
 
-        self.btn_gcp.setCheckable(True)
-        self.btn_gcp.clicked.connect(self.btn_gcp_function)
-
-    def btn_100_function(self):
-        if self.btn_100.isChecked():
-            self.btn_200.setEnabled(False)
-            self.btn_gcp.setEnabled(False)
-            print("Button 100 Pressed")
-        else:
-            self.btn_200.setEnabled(True)
-            self.btn_gcp.setEnabled(True)
-            print("Button 100 Released")
-
-    def btn_200_function(self):
-        if self.btn_200.isChecked():
-            self.btn_100.setEnabled(False)
-            self.btn_gcp.setEnabled(False)
-            print("Button 200 Pressed")
-        else:
-            self.btn_100.setEnabled(True)
-            self.btn_gcp.setEnabled(True)
-            print("Button 200 Released")
-
-    def btn_gcp_function(self):
-        if self.btn_gcp.isChecked():
-            self.btn_100.setEnabled(False)
-            self.btn_200.setEnabled(False)
-            print("Button GCP Pressed")
-        else:
-            self.btn_100.setEnabled(True)
-            self.btn_200.setEnabled(True)
-            print("Button GCP Released")
+    def pBtn100_collect_function(self):
+        print("100 Collect Pressed")
+        self.ip_list = []
+        start_ip_host = int(self.sip1004.text())
+        last_ip_host = int(self.lip1004.text())
+        for i in range(start_ip_host, last_ip_host+1):
+            input_ip = self.sip1001.text()+"."+self.sip1002.text()+"." + \
+                self.sip1003.text()+"."+str(i)
+            self.ip_list.append(input_ip)
+        print("IP List: ", self.ip_list)
 
 
 # QApplication : Run App
